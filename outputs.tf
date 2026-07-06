f478765aa3c9d7c825ef205a7560de9f10d972a8
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "api_management_notification_recipient_emails" {
   description = "All api_management_notification_recipient_email resources"
   value       = azurerm_api_management_notification_recipient_email.api_management_notification_recipient_emails
 }
+output "api_management_notification_recipient_emails_api_management_id" {
+  description = "List of api_management_id values across all api_management_notification_recipient_emails"
+  value       = [for k, v in azurerm_api_management_notification_recipient_email.api_management_notification_recipient_emails : v.api_management_id]
+}
+output "api_management_notification_recipient_emails_email" {
+  description = "List of email values across all api_management_notification_recipient_emails"
+  value       = [for k, v in azurerm_api_management_notification_recipient_email.api_management_notification_recipient_emails : v.email]
+}
+output "api_management_notification_recipient_emails_notification_type" {
+  description = "List of notification_type values across all api_management_notification_recipient_emails"
+  value       = [for k, v in azurerm_api_management_notification_recipient_email.api_management_notification_recipient_emails : v.notification_type]
+}
 
