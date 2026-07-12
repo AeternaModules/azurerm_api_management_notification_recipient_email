acc3b5b22c696945d67370b73c4d52b71f69d6b5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:16 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "api_management_notification_recipient_emails_id" {
+  description = "Map of id values across all api_management_notification_recipient_emails, keyed the same as var.api_management_notification_recipient_emails"
+  value       = { for k, v in azurerm_api_management_notification_recipient_email.api_management_notification_recipient_emails : k => v.id }
+}
 output "api_management_notification_recipient_emails_api_management_id" {
   description = "Map of api_management_id values across all api_management_notification_recipient_emails, keyed the same as var.api_management_notification_recipient_emails"
   value       = { for k, v in azurerm_api_management_notification_recipient_email.api_management_notification_recipient_emails : k => v.api_management_id }
